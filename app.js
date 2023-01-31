4314647a5619933fbcbe17bcec6b2ef6dc3d9112
RichardObiora <richardobiora24@gmail.com>
2023-01-31 07:24:37 +0100

    To do list angela yu

---
+++ app.js
@@ -0,0 +1,51 @@
+
+//jshint esversion:6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+
+
+const app = express();
+const items = ["Buy Food", "Eat Food", "Cook Food"];
+const workItems = [];
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+
+    const day = date.getDate();
+    
+        res.render("list", {listTitle: day, newListItems: items});
+});
+
+app.post("/", function(req, res){
+    const item = req.body.newItem;
+
+    if(req.body.list === "Work"){
+        workItems.push(item);
+        res.redirect("/work");
+    } else{
+        items.push(item)
+    res.redirect("/work");
+    };
+
+});
+
+app.get("/work", function(req, res) {
+    res.render("list", {listTitle: "Work List", newListItems: workItems});
+});
+
+app.get("/about", function(req, res){
+    res.render("about");
+});
+
+
+
+app.listen(3000, function(){
+    console.log("Your Server is now running on port 3000");
+});
